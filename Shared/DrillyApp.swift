cf97bbcafda1ea4e6e726777d11bf0a75b332ebf
Vitor Capretz <capretzvitor@gmail.com>
2021-05-08 15:02:25 +0200

    Initial Commit

---
+++ Shared/DrillyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrillyApp.swift
+//  Shared
+//
+//  Created by Vitor Capretz on 2021-05-08.
+//
+
+import SwiftUI
+
+@main
+struct DrillyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
